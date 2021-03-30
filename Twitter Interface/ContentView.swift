8c7315d1d51f1057304295bef5539c96053c6219
Tufael Ahmed <tahmed0113@gmail.com>
2021-03-30 01:03:40 -0400

    Initial Commit

---
+++ Twitter Interface/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Twitter Interface
+//
+//  Created by Ahmed on 3/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
